6b7ad723824c407250c37972ac66ea37073a9a9f
Nishant <Nishant.nawarkhede@gmail.com>
2015-03-11 23:19:18 +0530

    Created new appengine project

---
+++ testapp/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+import dbindexer
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+# django admin
+admin.autodiscover()
+
+# search for dbindexes.py in all INSTALLED_APPS and load them
+dbindexer.autodiscover()
+
+urlpatterns = patterns('',
+    #('^admin/', include(admin.site.urls)),
+)
